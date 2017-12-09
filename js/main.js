e9f0e576bac4ab1479a9dc49b31da64cab2ceb5f
Julia <jullien.fall@gmail.com>
2017-12-09 15:07:13 +0100

    doneee

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
 $(document).ready(function(){
-    
-    $(window).scroll(function(){
-        if ($(window).scrollTop() >= 20) {
-            $('.navbar').css('background-color', 'rgba(14, 216, 214, 0.7)');
-        } else {
-            $('.navbar').css('background-color', 'transparent');
-        }
-    });
-    
+    scrollWindow();
 });
+
+$(window).scroll(function(){
+    scrollWindow();
+}); 
+   
+
+function scrollWindow(){
+    var navbarHeight = $('.navbar').height(); 
+    
+     if ($(window).scrollTop() >= navbarHeight ) {
+         $('.navbar').addClass('drop-menu');
+         $('.navbar.navbar-light .nav-link').addClass('nav-link-hover');
+         $('.navbar .navbar-brand').addClass('navbar-brand-hover');
+    } else {
+        $('.navbar').removeClass('drop-menu');
+        $('.navbar.navbar-light .nav-link').removeClass('nav-link-hover');
+        $('.navbar .navbar-brand').removeClass('navbar-brand-hover');
+    }
+};
+    
